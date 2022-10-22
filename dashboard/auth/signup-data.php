aa5e979dc2f1ce5f8bcf256001aff8dc8be49983
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2022-10-22 20:47:59 +0600

    name and email field validation added

--- dashboard/auth/signup-data.php
+++ dashboard/auth/signup-data.php
@@ -1,26 +1,53 @@
 <?php
 session_start();
-$name = $_POST['name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$Cpassword = $_POST['confirm-password'];
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
+$password = htmlspecialchars($_POST['password']);
+$Cpassword = htmlspecialchars($_POST['confirm-password']);
 
 $flag = false;
+
+// name field validation logic 
 if($name){
 	$whitespace_slice = str_replace(" ", "", $name);
-if(ctype_alpha($whitespace_slice)){
-	echo $name;
+	if(!ctype_alpha($whitespace_slice)){
+		$_SESSION['name_value'] = $name;
+		$flag= true;
+		$_SESSION['name_error'] = 'Name must be String!';
+	}else{
+		echo $name;
+	}
 }else{
 	$flag= true;
-	$_SESSION['name_error'] = 'name must be String!';
-}	
+	$_SESSION['name_error'] = 'Name field required!';
+}
+
+
+
+// email field validation logic 
+if($email){
+	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+		$_SESSION['email_value'] = $email;
+		$flag= true;
+		$_SESSION['email_error'] = 'Invalid email!';
+	}else{
+	echo $email;
+	}
 
 }else{
 	$flag= true;
-	$_SESSION['name_error'] = 'name field required!';
-	
+	$_SESSION['email_error'] = 'Email field required!';
 }
+
+
+
 if($flag){
 	header('location:./signup.php');
+	if($name){
+		$_SESSION['name_value'] = $name;
+	}
+	if($email){
+	$_SESSION['email_value'] = $email;	
+	}
 }
 ?>
--- dashboard/auth/signup.php
+++ dashboard/auth/signup.php
@@ -17,21 +17,49 @@ session_start();
 	<div class="auth-credentials m-b-xxl">
 		<label for="signUpUsername" class="form-label">Name</label>
 		<input name="name" type="text" 
-		class="form-control m-b-md <?=isset($_SESSION['name_error']) ? 'is-invalid': ''?>"
-		 id="signUpUsername" aria-describedby="signUpUsername" placeholder="Enter Name">
-		<!-- error Message  -->
-		<?php
-		if(isset($_SESSION['name_error'])){
+		<?php 
+		if(isset($_SESSION['name_value'])){
 			?>
-			<p class="text-danger"><?= $_SESSION['name_error']?></p>
+			value="<?=$_SESSION['name_value']?> "
 			<?php
 		}
-		unset($_SESSION['name_error']);
+		unset($_SESSION['name_value']);
+		 ?>
+		class="form-control m-b-md <?=isset($_SESSION['name_error']) ? 'is-invalid': ''?>"
+		 id="signUpUsername" aria-describedby="signUpUsername" placeholder="Enter Name">
+		<!-- name error Message  -->
+		<?php
+		if(isset($_SESSION['name_value'])){
+			?>
+			<p class="text-danger"><?= $_SESSION['name_value']?></p>
+			<?php
+		}
+		unset($_SESSION['name_value']);
 
 		?>
 		<label for="signUpEmail" class="form-label">Email address</label>
-		<input type="email" name="email" class="form-control m-b-md" id="signUpEmail" aria-describedby="signUpEmail" placeholder="example@neptune.com">
+		<input type="text"
+		<?php 
+		if(isset($_SESSION['email_value'])){
+			?>
+			value="<?=$_SESSION['email_value']?> "
+			<?php
+		}
+		unset($_SESSION['email_value']);
+		 ?>
+		 name="email"
+		 class="form-control m-b-md <?=isset($_SESSION['email_error']) ? 'is-invalid': ''?>"
+		  id="signUpEmail" aria-describedby="signUpEmail" placeholder="example@neptune.com">
+		  <!-- email error message  -->
+		 <?php
+		if(isset($_SESSION['email_error'])){
+			?>
+			<p class="text-danger"><?= $_SESSION['email_error']?></p>
+			<?php
+		}
+		unset($_SESSION['email_error']);
 
+		?>
 		<label for="signUpPassword"  class="form-label">Password</label>
 		<input type="password" name="password" class="form-control" id="signUpPassword" aria-describedby="signUpPassword" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
 		<div id="emailHelp" class="form-text m-b-md">Password must be minimum 8 characters length*</div>
