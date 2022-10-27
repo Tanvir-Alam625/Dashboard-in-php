e6dcc123d9b3c3581513a3c813eded9194ed1c76
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2022-10-27 12:54:02 +0600

    today all done

--- dashboard/auth/signin-data.php
+++ dashboard/auth/signin-data.php
@@ -1,23 +1,33 @@
 <?php
+session_start();
 require_once('../db_connect/db_connect.php');
     $email = htmlspecialchars($_POST['email']);
-    $password  = sha1(htmlspecialchars($_POST['password']));
+    $password  = htmlspecialchars($_POST['password']);
+    $hashed_password = sha1($password);
     $flag = false;
+
+    // query checking 
+    $signin_query = "SELECT COUNT(*) as 'result' FROM users WHERE Email='$email' AND Password='$hashed_password';";
+    $signin_query_check = mysqli_query($db_connect, $signin_query);
+    $signin_query_result = mysqli_fetch_assoc($signin_query_check);
+    echo $signin_query_result['result'];
+    if($signin_query_result['result'] == 1){
+        header('location: ../index.php');
+    }else{
+        $flag = true;
+        $_SESSION["signin_error"]= "Wrong credentials!";
+    }
+    // form validation 
     if(!$email){
             $flag = true;
-            $_SESSION["email_error"]= "Email Field Required!";
+            $_SESSION["signin_email_error"]= "Email Field Required!";
     }
     if(!$password){
         $flag = true;
-        $_SESSION["password_error"]= "Password Field Required!";
+        $_SESSION["signin_password_error"]= "Password Field Required!";
     }
     if($flag){
         header('location: ./signin.php');
-    }else{
-        $signin_query = "SELECT COUNT(*) as 'result' FROM users WHERE Email='$email' AND Password='$password';";
-        $signin_query_check = mysqli_query($db_connect, $signin_query);
-        $signin_query_result = mysqli_fetch_assoc($signin_query_check);
-        echo $signin_query_result['result'];
     }
 
 ?>
--- dashboard/auth/signin.php
+++ dashboard/auth/signin.php
@@ -1,5 +1,6 @@
 <?php
 require_once('../includes/header.php');
+session_start();
 ?>
 
 <div class="app app-auth-sign-in align-content-stretch d-flex flex-wrap justify-content-end">
@@ -10,13 +11,49 @@ require_once('../includes/header.php');
             <a href="index.html">Neptune</a>
             </div>
             <p class="auth-description">Please sign-in to your account and continue to the dashboard.<br>Don't have an account? <a href="signup.php">Sign Up</a></p>
+            <!-- signin error message  -->
+            <?php
+            if(isset($_SESSION['signin_error'])){?>
+            <div class="d-flex justify-content-center">
+                <div class="w-50 p-2 d-flex justify-content-center align-items-center mx-auto rounded border border-danger " style="background:#f3011436;" role="">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="20px" width="20px" class="text-danger ">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
 
+                    <p class="text-danger text-center ml-10 my-0 p-0"><?=$_SESSION['signin_error']?></p>
+                </div>
+            </div>
+            <?php
+            }
+            session_unset();
+            ?>
             <div class="auth-credentials m-b-xxl">
                 <label for="signInEmail" class="form-label">Email address</label>
-                <input name="email" type="email" class="form-control m-b-md" id="signInEmail" aria-describedby="signInEmail" placeholder="example@neptune.com">
-
+                <input name="email" type="email"
+                class="form-control <?=isset($_SESSION['signin_email_error']) ? 'is-invalid': ''?>" 
+                 id="signInEmail" aria-describedby="signInEmail" placeholder="example@neptune.com">
+                <!-- email error Message  -->
+                <?php
+                if(isset($_SESSION['signin_email_error'])){
+                    ?>
+                    <p class="text-danger"><?= $_SESSION['signin_email_error']?></p>
+                    <?php
+                }
+                unset($_SESSION['signin_email_error']);
+                ?>
                 <label for="signInPassword" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control" id="signInPassword" aria-describedby="signInPassword" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
+                <input type="password" name="password" 
+                class="form-control  <?=isset($_SESSION['signin_password_error']) ? 'is-invalid': ''?>"
+                id="signInPassword" aria-describedby="signInPassword" >
+                <!-- email error Message  -->
+                <?php
+                if(isset($_SESSION['signin_password_error'])){
+                    ?>
+                    <p class="text-danger"><?= $_SESSION['signin_password_error']?></p>
+                    <?php
+                }
+                session_unset();
+                ?>
             </div>
 
             <div class="auth-submit">
